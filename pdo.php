77f9b16a9009811dccc27c07f5e18faf61298f92
Rakshit GL <35193981+Raks110@users.noreply.github.com>
2018-09-13 11:36:08 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -4,8 +4,8 @@
 	{
 		$host_name = 'localhost';
 		$db_name='login';
-		$user='root';
-		$pass='m@st3rk!k0';
+		$user='seven';
+		$pass='zap';
 		$pdo= new PDO("mysql:host=".$host_name.";dbname=".$db_name,$user,$pass);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
